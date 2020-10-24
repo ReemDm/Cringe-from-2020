b82b6e9a2d4a1579a7b4464a8d4779f9ab6a08f6
ReemDm <71402771+ReemDm@users.noreply.github.com>
2020-10-24 16:34:18 +0500

    Мой пробный проект №1 (не доделана корзина)
    24.10.2020

---
+++ Script.js
@@ -0,0 +1,14 @@
+let car
+
+
+function buyProduct() {
+    let result = confirm('Уверен в покупке?');
+
+    if (result==true) {
+        
+    }
+    else {
+        alert('Не правильно')
+    }
+
+}
